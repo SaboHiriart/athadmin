c3a975353634590002f32dc6264d40d175c8137f
Sebastian Ochoa Hiriart <sebastian@GH-LAP006.hazesa.intranet>
2021-12-13 11:14:55 -0600

    Change in functionality

--- view-login/renderer.js
+++ view-login/renderer.js
@@ -1,15 +1,60 @@
-const mysql = require('../database.js')
+const dialog = require('electron').remote.dialog;
+const db = require('../database.js')
 
-let user = document.getElementById('inpUser');
+const inpUser = document.getElementById('inpUser');
+const inpPassword = document.getElementById('inpPassword');
+const btnLogin = document.getElementById('btnLogin');
+const imgProfile = document.getElementById('imgProfile');
 
-//const changeImg = function(){
-//    var where = "user='" + user.value() + "'";
-//    console.log(mysql.select('*', 'users', where, true));
-//}
-//user.addEventListener('propertychange', changeImg);
 
-var result = mysql.select('*', 'users', '', false);
-Object.keys(result).forEach(function(key){
-    var row = result[key];
-    console.log(row.nombre);
+//Función que cambia la imagen de pefil al escribir el usuario
+const changeProfilePic = function(e){
+    var sql = "SELECT * FROM users WHERE user='" + inpUser.value + "'";
+    db.query(sql, function(err, result, fields){
+        if(err) throw err;
+        if(result.length >= 1){
+            console.log(result);
+            var source = result[0].user;
+            imgProfile.src="./assets/profilePics/" + source + ".jpg";
+        }
+    });
+}
+
+//Función que valida que los campos estén llenos
+const dataValidate = function(){
+    if(inpUser.value === ''){
+        dialog.showMessageBox({
+            title: 'Error en Credenciales',
+            buttons: ['OK'],
+            type: 'warning',
+            message: 'tesing'
+        })
+        return false;
+    }else if(inpPassword.value === ''){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+//Eventos para el input de Usuario
+inpUser.addEventListener('input', changeProfilePic);
+inpUser.addEventListener('propertychange', changeProfilePic);
+inpUser.addEventListener('keypress', function(event){
+    if(event.keyCode === 13){
+        event.preventDefault();
+        inpPassword.focus();
+    }
 });
+
+//Eventos para el input de Password
+inpPassword.addEventListener('keypress', function(event){
+    if(event.keyCode === 13){
+        event.preventDefault();
+        btnLogin.click();
+    }
+});
+
+btnLogin.onclick = function(){
+    dataValidate();
+}
