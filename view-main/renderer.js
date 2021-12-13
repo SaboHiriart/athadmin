5dae06c85a530f5014c9f303ab89e54d2be5b504
Sebastian Ochoa Hiriart <sebastian@GH-LAP006.hazesa.intranet>
2021-12-13 17:46:11 -0600

    main view design

---
+++ view-main/renderer.js
@@ -0,0 +1,17 @@
+const btnUsuarios = document.getElementById("btnUsuarios");
+
+btnUsuarios.addEventListener("click", openTab(event, 'tabUsuarios'))
+
+function openTab(evt, tabname) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
