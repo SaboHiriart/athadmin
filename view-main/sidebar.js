43a1e7ed34e41dd97a29036e0f1c9f07234327cb
Sebastian Ochoa Hiriart <sebastian@GH-LAP006.hazesa.intranet>
2021-12-17 09:35:40 -0600

    Cambios en permisos a tabs

--- view-main/sidebar.js
+++ view-main/sidebar.js
@@ -1,10 +1,43 @@
-window.onload = function() {
+const db = require("../database");
+
+const moduleValidate = function (idTab, callback) {
+  var id_module = idTab;
+  var id_user = 2;
+  var sql =
+    "SELECT * FROM user_privilege WHERE module_id=" +
+    id_module +
+    " AND user_id=" +
+    id_user;
+  db.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    if (result.length > 0) {
+      if (result[0].allow === 1) {
+        callback(true);
+      } else {
+        callback(false);
+      }
+    } else {
+      callback(false);
+    }
+  });
+};
+
+window.onload = function () {
   document.getElementById("homePage").style.display = "block";
 };
 
-function openTab(evt, tabName){
-     var i, tabcontent, tablinks;
+function openTab(evt, tabName) {
+  var tabId;
+  if(tabName === 'addUser'){
+    tabId = 2;
+  }else if(tabName === 'searchUser'){
+    tabId = 3;
+  }else if(tabName === 'modifyUser'){
+    tabId = 4;
+  }
 
+  moduleValidate(tabId, function (resultado) {
+    var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
@@ -17,4 +50,5 @@ function openTab(evt, tabName){
 
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
+  });
 }
