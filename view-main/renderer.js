f048497587fb9b1010ea7ec3100034214e2238cb
Sebastian Ochoa Hiriart <sebastian@GH-LAP006.hazesa.intranet>
2021-12-24 09:23:01 -0600

    Cambios en la forma de abrir las tabs

--- view-main/renderer.js
+++ view-main/renderer.js
@@ -1,8 +1,20 @@
+const Webcam = require("webcamjs");
+const fs = require("fs");
+
+const clockHomePage = document.getElementById("clockHomePage");
+const nombreHomePage = document.getElementById("nombreHomePage");
 const btnSubmenuUsers = document.getElementById("btnSubmenuUsers");
 const icoSubmenuUsers = document.getElementById("icoSubmenuUsers");
 
+//Add User Objects
+const btnStartCamAdduser = document.getElementById("btnStartCamAddUser");
+const btnTakePictureAddUser = document.getElementById("btnTakePictureAddUser");
+const camAddUser = document.getElementById("camAddUser");
+const picAddUser = document.getElementById("picAddUser");
+
 var userExpand = false;
 
+//Funciones para los iconos de la sidebar
 btnSubmenuUsers.onclick = function () {
   if (userExpand === false) {
     icoSubmenuUsers.classList.remove("fa-plus-square");
@@ -14,4 +26,59 @@ btnSubmenuUsers.onclick = function () {
     userExpand = false;
     console.log(userExpand);
   }
+};
+
+//Funciones para Home Tab
+setInterval(showTitles, 1000);
+function showTitles() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  let date = time.getDate();
+  let month = time.getMonth() + 1;
+  let year = time.getFullYear();
+  am_pm = " AM";
+
+  if (hour > 12) {
+    hour -= 12;
+    am_pm = " PM";
+  }
+  if (hour == 0) {
+    hr = 12;
+    am_pm = " AM";
+  }
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+  let currentTime = hour + ":" + min + ":" + sec + am_pm;
+  let currentDate = date + "/" + month + "/" + year;
+  clockHomePage.innerHTML = currentDate + "<br>" + currentTime;
+  nombreHomePage.innerHTML = "Bienvenido " + name_user;
+}
+showTitles();
+
+//Funciones para AddUser Tab
+var base64ImageAddUser;
+
+btnStartCamAdduser.onclick = function () {
+  picAddUser.classList.add("d-none");
+  camAddUser.classList.remove("d-none");
+  Webcam.set({
+    width: 320,
+    heigth: 220,
+    image_format: "jpeg",
+    jpeg_quality: 90,
+  });
+  Webcam.attach("camAddUser");
+};
+
+btnTakePictureAddUser.onclick = function () {
+  Webcam.snap(function(data_uri){
+    picAddUser.innerHTML =  '<img src="'+data_uri+'"/>';
+    base64ImageAddUser = data_uri;
+  });
+  picAddUser.classList.remove("d-none")
+  Webcam.reset();
+  camAddUser.classList.add("d-none");
 };
--- view-main/sidebar.js
+++ view-main/sidebar.js
@@ -1,12 +1,8 @@
 const { ipcRenderer } = require("electron");
 const db = require("../database");
 
-var id_user = 1;
-
-ipcRenderer.on('userDataSender', function(event, arg) {
-  alert(arg);
-  id_user = arg;
-});
+var id_user;
+var name_user;
 
 const moduleValidate = function (idTab, callback) {
   var id_module = idTab;
@@ -30,25 +26,30 @@ const moduleValidate = function (idTab, callback) {
   });
 };
 
-window.onload = function () {
+ipcRenderer.on("userDataSender", function (event, arg) {
+  id_user = arg;
+  var sql = "SELECT name FROM users WHERE user_id=" + id_user;
+  db.query(sql, function (err, result, field){
+    if(err) throw err;
+    name_user = result[0].name;
+  });
   openTab(event, 'homePage');
-};
+});
 
 function openTab(evt, tabName) {
   var tabId;
-  if(tabName === 'homePage'){
-    tabId=1;
-  }
-  else if(tabName === 'addUser'){
+  if (tabName === "homePage") {
+    tabId = 1;
+  } else if (tabName === "addUser") {
     tabId = 2;
-  }else if(tabName === 'searchUser'){
+  } else if (tabName === "modifyUser") {
     tabId = 3;
-  }else if(tabName === 'modifyUser'){
+  } else if (tabName === "deleteUser") {
     tabId = 4;
   }
 
   moduleValidate(tabId, function (resultado) {
-    if(resultado === true){
+    if (resultado === true) {
       var i, tabcontent, tablinks;
       tabcontent = document.getElementsByClassName("tabcontent");
       for (i = 0; i < tabcontent.length; i++) {
@@ -60,7 +61,6 @@ function openTab(evt, tabName) {
       }
       document.getElementById(tabName).style.display = "block";
       document.getElementById(tabName).classList.add("active");
-      document.getElementById(tabName).innerHTML = '<object type="text/html" data="./tabs/' + tabName +'/index.html" ></object>';
     }
   });
 }
