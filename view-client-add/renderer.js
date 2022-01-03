8fcbd70c767547f7726428cf72408d32d01f87ba
Sebastian Ochoa Hiriart <saboochoahiriart@outlook.com>
2022-01-03 17:54:06 -0600

    view for register clientes

---
+++ view-client-add/renderer.js
@@ -0,0 +1,17 @@
+const dpFechaNacimiento = document.getElementById("cumpleCliente");
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth();
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+    dd = '0' + dd;
+ }
+ 
+ if (mm < 10) {
+    mm = '0' + mm;
+ } 
+     
+ today = yyyy + '-' + mm + '-' + dd;
+dpFechaNacimiento.setAttribute("max", today);
