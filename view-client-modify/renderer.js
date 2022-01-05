f43bcbd7bd7f5efd0220bb41bbbb24be5d77e6cf
Sebastian Ochoa Hiriart <saboochoahiriart@outlook.com>
2022-01-05 12:15:42 -0600

    Declaracion de objetos para modificacion de informacion

---
+++ view-client-modify/renderer.js
@@ -0,0 +1,18 @@
+const nombreCliente = document.getElementById("nombreCliente");
+const apPatCliente = document.getElementById("apPatCliente");
+const apMatCliente = document.getElementById("apMatCliente");
+const fechaCliente = document.getElementById("fechaCliente");
+const telefonoCliente = document.getElementById("telefonoCliente");
+const correoCliente = document.getElementById("correoCliente");
+const nombreEmergenciaCliente = document.getElementById(
+  "nombreEmergenciaCliente"
+);
+const telefonoEmergenciaCliente = document.getElementById(
+  "telefonoEmergenciaCliente"
+);
+const disciplinaCliente = document.getElementById("disciplinaCliente");
+const btnStart = document.getElementById("btnStart");
+const btnTakePicture = document.getElementById("btnTakePicture");
+const picPhoto = document.getElementById("picPhoto");
+const camPhoto = document.getElementById("camPhoto");
+const btnGuardar = document.getElementById("btnGuardar");
